00660350211269061462aae1b1b57c12218a0ed2
hongphuclong <hongphuclongdl2003@gmail.com>
2024-01-05 15:05:20 +0700

    add dao database

---
+++ src/main/java/model/address.java
@@ -0,0 +1,52 @@
+package model;
+
+public class address {
+    private int userID;
+    private String city;
+    private String district;
+    private String ward;
+    private String detail;
+
+    public address() {
+    }
+
+    public int getUserID() {
+        return userID;
+    }
+
+    public void setUserID(int userID) {
+        this.userID = userID;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getDistrict() {
+        return district;
+    }
+
+    public void setDistrict(String district) {
+        this.district = district;
+    }
+
+    public String getWard() {
+        return ward;
+    }
+
+    public void setWard(String ward) {
+        this.ward = ward;
+    }
+
+    public String getDetail() {
+        return detail;
+    }
+
+    public void setDetail(String detail) {
+        this.detail = detail;
+    }
+}
---
+++ src/main/java/model/wishes.java
@@ -0,0 +1,25 @@
+package model;
+
+public class wishes {
+    private int userID;
+    private int productID;
+
+    public wishes() {
+    }
+
+    public int getUserID() {
+        return userID;
+    }
+
+    public void setUserID(int userID) {
+        this.userID = userID;
+    }
+
+    public int getProductID() {
+        return productID;
+    }
+
+    public void setProductID(int productID) {
+        this.productID = productID;
+    }
+}
